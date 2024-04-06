01e7923a8b1db5c24b19761f60cb7045b5127a42
Gabriel <96741685+gabrielkey@users.noreply.github.com>
2024-04-05 21:33:33 -0300

    Semana 06

---
+++ Semana 05/nodejs.js
@@ -0,0 +1,25 @@
+// let dataAtual = new Date().toString();
+
+// let vetorDeDatas = dataAtual.split(' ');
+// let vetorDeHoras = vetorDeDatas[4].split(':');
+
+// let dia = vetorDeDatas[2];
+// let mes = vetorDeDatas[1];
+// let ano = vetorDeDatas[3];
+// let hora = vetorDeHoras[0]
+// let minutos = vetorDeHoras[1];
+
+// console.log(`Data: ${dia} de ${mes} de ${ano}<br />Hora: ${hora}h${minutos}`);
+
+
+const pessoas = "Larissa Rayssa;João Neto;João Gabriel";
+
+let resultado = pessoas
+
+function minhaFuncao(msg) {
+    //Parametros (opcionais) === variaveis
+    //Argumentos === dados (valores atribuidos a parametros)
+    return msg;
+}
+
+console.log(minhaFuncao('Olá'));
